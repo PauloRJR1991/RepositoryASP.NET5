5b7d948cc1d238eede49539fcdc732990993add7
Paulo Junior <pauloetec@bol.com.br>
2023-01-15 21:01:25 -0300

    implemetando os verbos

--- RestWithASPNET/RestWithASPNET5/Controllers/PersonController.cs
+++ RestWithASPNET/RestWithASPNET5/Controllers/PersonController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RestWithASPNET5.Model;
+using RestWithASPNET5.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,17 +14,58 @@ namespace RestWithASPNET5.Controllers
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
-
-        public PersonController(ILogger<PersonController> logger)
+        private IPersonService _ipersonservice;
+        public PersonController(ILogger<PersonController> logger, IPersonService personservice)
         {
+            _ipersonservice = personservice;
             _logger = logger;
         }
 
-        [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Soma(string firstNumber, string secondNumber)
+        [HttpGet]
+        public IActionResult Get()
         {
-            return BadRequest("Inavalid Input");
+            return Ok(_ipersonservice.FindAll());
         }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _ipersonservice.FindByID(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return Ok(person);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_ipersonservice.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_ipersonservice.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _ipersonservice.Delete(id);
+
+            return NoContent();
+        }
+
 
     }
 }
